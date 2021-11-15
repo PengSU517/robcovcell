b6ed76d13930891fe7cc33638f0c122b74c05bcb
PengSu95 <61771120+PengSu95@users.noreply.github.com>
2021-11-15 14:10:21 +1100

    15/11/2021

--- R/cellweights.R
+++ R/cellweights.R
@@ -10,30 +10,53 @@ require(robustbase)
 #' @export
 #'
 #' @examples
-covCW = function(x, group = NULL){
+covCW = function(x){
   n = dim(x)[1]
   p = dim(x)[2]
 
-  if(is.null(group)){
-    grpup = 1:p
-  }
+  wts = matrix(1, nrow = n, ncol = p)
+  wts[!is.na(match(as.vector(x),boxplot(as.vector(x),plot=FALSE)$out))] = 0
+  wts = wts%*%diag(sqrt(n/colSums(wts)))
+  muhat = colSums(x*wts)/colSums(wts)
+  xcenter = x - rep(1,n)%*%t(muhat)
+  covmatrix = t(xcenter*wts)%*%(xcenter*wts)/n
 
-  wts = matrix(0, ncol = p, nrow = n)
-  for (i in unique(group)) {
-    labels = (group==i)
-    wts[,labels] = robustbase::covMcd(x[,labels])$mcd.wt
-  }
 
-  ww = sqrt(n/colSums(wts))
-  wtsa = wts%*%diag(ww)
-  xtilde = x*wtsa
-
-  covmatrix = t(xtilde)%*%xtilde/n
   scale = sqrt(diag(covmatrix))
   cormatrix = (diag(1/scale))%*%covmatrix%*%(diag(1/scale))
   return(list(covmatrix = covmatrix, cormatrix = cormatrix))
 
 }
+
+
+## regression will cellwise weights
+#' Title
+#'
+#' @param x
+#' @param group
+#'
+#' @return
+#' @export
+#'
+#' @examples
+covBC = function(x, group){
+  n = dim(x)[1]
+  p = dim(x)[2]
+
+  covmatrix = robcovsel::covf(x, cor.method = "pair",
+                              scale.method = "qn", pda.method = F)$covmatrix
+  covmatrix[group!=3, group!=3] = robustbase::covMcd(x[,group!=3])$cov
+  covmatrix[group==1, group==1] = cov(x[,group==1])
+  scale = sqrt(diag(covmatrix))
+  cormatrix = (diag(1/scale))%*%covmatrix%*%(diag(1/scale))
+
+  return(list(covmatrix = covmatrix, cormatrix = cormatrix))
+
+}
+
+
+
+
 
 
 
--- R/helper.R
+++ R/helper.R
@@ -16,15 +16,15 @@
 #' x = dat$x
 
 
-genevar = function(n = 100, p = 20, e = 0.05, r = 0.5, gamma = 10, group = NULL){
+genevar = function(n = 100, p = 15, e = 0.05, r = 0.5, gamma = 10, group = NULL){
 
   if(is.null(group)){
-    size = p/4
-    group = c(rep(1,size), rep(2,size), rep(3,size), 4:(3+size))
+    size = p/3
+    group = c(rep(1,size), rep(2,size), rep(3,size))
   }
 
   {
-    mu = rep(0,p)
+    mu = rep(10,p)
     sigma = diag(rep(1^2,p))
     for (i in 1:p) {for (j in 1:p) {
       if (i !=j)sigma[i,j] = sqrt(sigma[i,i]*sigma[j,j])*r^abs(i-j)}}
@@ -34,15 +34,15 @@ genevar = function(n = 100, p = 20, e = 0.05, r = 0.5, gamma = 10, group = NULL)
     xr = mvtnorm::rmvnorm(n = n, mean = mu, sigma = sigma)
     bi1 = matrix(0, nrow = n, ncol = size)
     bi2 = matrix(sample(c(rep(1,e*n),rep(0,((1-e)*n)))), nrow = n, ncol = size)
-    bi3 = matrix(sample(c(rep(1,e*n),rep(0,((1-e)*n)))), nrow = n, ncol = size)
-    bi4 = apply(matrix(0, nrow = n, ncol = size), 2,
+    #bi3 = matrix(sample(c(rep(1,e*n),rep(0,((1-e)*n)))), nrow = n, ncol = size)
+    bi3 = apply(matrix(0, nrow = n, ncol = size), 2,
                 function(xvec) {xvec[sample(x = 1:n, size = e*n)] = 1; return(xvec)})
-    bi = cbind(bi1, bi2, bi3, bi4)
+    bi = cbind(bi1, bi2, bi3)
     outl = rnorm(n = n*p, mean = gamma, sd = 1)
     rsign = sample(c(-1,1), size = n*p, replace = T)
     outlier = matrix(outl*rsign, nrow = n, ncol=p)
     x = xr*(1-bi)+outlier*bi
   }
-  return(list(x = x, group = group))
+  return(list(x = x, group = group, sigma = sigma, bi = bi))
 
 }
