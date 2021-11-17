d8ca0cc943d94ae4ff13c78f9e5336aa782ee703
PengSu95 <61771120+PengSu95@users.noreply.github.com>
2021-11-17 18:34:10 +1100

    17/11/2021

--- R/helper.R
+++ R/helper.R
@@ -16,33 +16,83 @@
 #' x = dat$x
 
 
-genevar = function(n = 100, p = 15, e = 0.05, r = 0.5, gamma = 10, group = NULL){
+genevar = function(n = 100, p = 15, e = 0.2, r = 0.9, gamma = 10, mu, sigma, type){
 
-  if(is.null(group)){
-    size = p/3
-    group = c(rep(1,size), rep(2,size), rep(3,size))
-  }
-
-  {
+  if(is.null(mu)){
     mu = rep(10,p)
     sigma = diag(rep(1^2,p))
     for (i in 1:p) {for (j in 1:p) {
       if (i !=j)sigma[i,j] = sqrt(sigma[i,i]*sigma[j,j])*r^abs(i-j)}}
   }
 
-  {
-    xr = mvtnorm::rmvnorm(n = n, mean = mu, sigma = sigma)
+  if(type == "blocked"){
+    size = p/3
+
     bi1 = matrix(0, nrow = n, ncol = size)
     bi2 = matrix(sample(c(rep(1,e*n),rep(0,((1-e)*n)))), nrow = n, ncol = size)
-    #bi3 = matrix(sample(c(rep(1,e*n),rep(0,((1-e)*n)))), nrow = n, ncol = size)
     bi3 = apply(matrix(0, nrow = n, ncol = size), 2,
                 function(xvec) {xvec[sample(x = 1:n, size = e*n)] = 1; return(xvec)})
     bi = cbind(bi1, bi2, bi3)
+
     outl = rnorm(n = n*p, mean = gamma, sd = 1)
     rsign = sample(c(-1,1), size = n*p, replace = T)
     outlier = matrix(outl*rsign, nrow = n, ncol=p)
-    x = xr*(1-bi)+(outlier+10)*bi
   }
-  return(list(x = x, group = group, sigma = sigma, bi = bi))
+
+  if(type == "structured"){
+    bi = apply(matrix(FALSE, nrow = n, ncol = p), 2,
+               function(xvec) {xvec[sample(x = 1:n, size = e*n)] = TRUE; return(xvec)})
+    gene = function(labels){
+      #labels = bi[1,]
+      k = sum(labels)
+      outl = rep(0,p)
+
+      if(k!=0){
+        muk = mu[labels]
+        sigmak = sigma[labels, labels]
+        u = eigen(sigmak)$vectors[,k]
+        v = gamma*sqrt(k)*t(u)/mahalanobis(u+muk, muk, sigmak)
+        outl[labels] = v
+      }
+
+      return(outl)
+    }
+
+    outlier = t(apply(bi, 1, gene))
+    dim(outlier)
+
+  }
+
+  if(type == "cellwise"){
+    bi = apply(matrix(0, nrow = n, ncol = p), 2,
+                function(xvec) {xvec[sample(x = 1:n, size = e*n)] = 1; return(xvec)})
+    outl = rnorm(n = n*p, mean = gamma, sd = 1)
+    rsign = sample(c(-1,1), size = n*p, replace = T)
+    outlier = matrix(outl*rsign, nrow = n, ncol=p)
+  }
+
+  {
+    xr = mvtnorm::rmvnorm(n = n, mean = mu, sigma = sigma)
+    x = xr*(1-bi)+(outlier+mu)*bi
+  }
+
+
+  return(list(x = x, sigma = sigma, bi = bi))
 
 }
+
+
+KLdiv = function(x0, xhat){
+  value = matrixcalc::matrix.trace(xhat%*%solve(x0)) - log(det(xhat%*%solve(x0))) - dim(x0)[1]
+  return(value)
+}
+
+
+
+
+
+
+
+
+
+
